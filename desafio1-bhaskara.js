d730a7888724d46a549ea5bbe12ffa96e4d39e99
João Victor <70175963+victrslx@users.noreply.github.com>
2025-02-12 10:50:20 -0300

    Update desafio1-bhaskara.js

--- desafio1-bhaskara.js
+++ desafio1-bhaskara.js
@@ -1,20 +1,20 @@
 function bhaskara(a, b, c) {
-    if (a === 0) { // se 'a' for igual a zero, a equação não é de segundo grau
+    if (a === 0) { 
         return "'a' não pode ser igual a zero.";
     }
 
-    let delta = b ** 2 - 4 * a * c; // delta = b² - 4ac, onde a, b e c são os coeficientes da equação de segundo grau
+    let delta = b ** 2 - 4 * a * c; 
 
-    if (delta < 0) { // se delta for menor que zero, a equação não possui raízes reais
+    if (delta < 0) { 
         return "nao possui raizes reais";
-    }else if (delta === 0) { // se delta for igual a zero, a equação possui apenas uma raiz real
+    }else if (delta === 0) { 
         let x = -b / (2 * a);
-        return { mensagem: "só tem uma raiz real", raiz: x }; // retorna um objeto com a mensagem e a raiz
+        return { mensagem: "só tem uma raiz real", raiz: x }; 
     }else{
-        let raizDelta = Math.sqrt(delta); // raiz quadrada de delta
-        let x1 = (-b + raizDelta) / (2 * a); // primeira raiz
-        let x2 = (-b - raizDelta) / (2 * a); // segunda raiz 
-        return { x1, x2 }; // retorna um objeto com as duas raízes
+        let raizDelta = Math.sqrt(delta); 
+        let x1 = (-b + raizDelta) / (2 * a); 
+        let x2 = (-b - raizDelta) / (2 * a); 
+        return { x1, x2 }; 
     }
 }
 console.log(bhaskara(1, -3, 2)); 
